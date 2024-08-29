c76aea6e0e0491a1034ccb46ad4110499f8c3a82
Xerx81 <ryuvraj832@gmail.com>
2024-08-29 12:55:08 +0530

    fixed mason-lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -81,15 +81,5 @@ return {
 	},
 	{
 		'williamboman/mason-lspconfig.nvim',
-		config = function()
-			require('mason-lspconfig').setup({
-				ensure_installed = {'pyright'},
-				handlers = {
-					function(server_name)
-						require('lspconfig')[server_name].setup({})
-					end,
-				},
-			})
-		end
 	}
 }
