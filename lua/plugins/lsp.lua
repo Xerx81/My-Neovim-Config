7670ac9014bfe6c3b555f8013e99334752886b33
Xerx81 <ryuvraj832@gmail.com>
2025-01-25 14:01:52 +0530

    added eslint to lsp and typescript to treesitter

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -30,6 +30,7 @@ return {
                 "pyright",
                 "clangd",
                 "ts_ls",
+                "eslint",
                 "html",
                 "cssls",
             },
