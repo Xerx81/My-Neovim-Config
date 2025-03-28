40c9c6f719035bef4c694858a5d5b83d43167ce9
Xerx81 <ryuvraj832@gmail.com>
2025-03-28 18:15:02 +0530

    disabled treesitter highliter for c

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -21,6 +21,7 @@ return {
             highlight = {
                 -- `false` will disable the whole extension
                 enable = true,
+                disable = { "c" },
 
                 -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
                 -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
