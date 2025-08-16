7d65b01c273ca961497d9a7752c5792bda5a2556
Xerx81 <ryuvraj832@gmail.com>
2025-08-16 13:12:05 +0530

    changed colorscheme to dracula

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,15 @@
 return {
 	{
         -- "rose-pine/neovim", name = "rose-pine",
-        -- "EdenEast/nightfox.nvim",
         -- "ellisonleao/gruvbox.nvim",
-        "folke/tokyonight.nvim",
+        -- "folke/tokyonight.nvim",
+        "Mofiqul/dracula.nvim",
         -- "catppuccin/nvim", name = "catppuccin",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
+			vim.cmd([[colorscheme dracula-soft]])
 
 			vim.api.nvim_set_hl( 0, "Normal", { bg = "none"})
 			vim.api.nvim_set_hl( 0, "NormalFloat", { bg = "none"})
