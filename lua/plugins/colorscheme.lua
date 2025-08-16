82dde823818daa81f47dfc4384668ee049d10b06
Xerx81 <ryuvraj832@gmail.com>
2025-08-16 12:43:21 +0530

    changed colorscheme to tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,12 +3,13 @@ return {
         -- "rose-pine/neovim", name = "rose-pine",
         -- "EdenEast/nightfox.nvim",
         -- "ellisonleao/gruvbox.nvim",
-        "catppuccin/nvim",
+        "folke/tokyonight.nvim",
+        -- "catppuccin/nvim", name = "catppuccin",
         lazy = false, -- make sure we load this during startup if it is your main colorscheme
 		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- load the colorscheme here
-			vim.cmd([[colorscheme catppuccin]])
+			vim.cmd([[colorscheme tokyonight]])
 
 			vim.api.nvim_set_hl( 0, "Normal", { bg = "none"})
 			vim.api.nvim_set_hl( 0, "NormalFloat", { bg = "none"})
